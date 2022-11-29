2dfb4170e9ef5095c677ca2698ac3ea1de575d51
alistairnathanbrown <alistairnathanbrown@icloud.com>
2022-11-29 16:34:14 +0000

    innitial version with uniq test passing

--- src/applications/uniq.py
+++ src/applications/uniq.py
@@ -7,4 +7,34 @@ from glob import glob
 
 
 def uniq(input, output, args):
-    return
+    if len(args) > 2:
+        raise ValueError("wrong number of command line arguments")
+    if len(args) == 1:
+        file = args[0]
+        case = 0
+    if len(args) == 2:
+        if args[0] != "-i":
+            raise ValueError("wrong flags")
+        else:
+            case = 1
+            file = args[1]
+
+    with open(file, "r") as f:
+        contents = f.read().splitlines()
+
+    indexToRemove = []
+
+    if case == 0:
+        for i in range(0, len(contents) - 1):
+            if contents[i] == contents[i + 1]:
+                indexToRemove.append(i + 1)
+    elif case == 1:
+        for i in range(0, len(contents) - 1):
+            if contents[i].lower() == contents[i + 1].lower():
+                indexToRemove.append(i + 1)
+
+    for index in indexToRemove:
+        contents.pop(index)
+
+    for line in contents:
+        output.append(line + "\n")
