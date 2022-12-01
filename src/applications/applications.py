9a32d0ce44764ecece8b7460684fb4618af86206
charliebarber <charlieb@protonmail.com>
2022-12-01 15:44:36 +0000

    CD bug fixed

--- src/applications/applications.py
+++ src/applications/applications.py
@@ -54,10 +54,11 @@ class Cd(Application):
     def __init__(self) -> None:
         pass
 
-    def exec(self, args, input) -> None:
+    def exec(self, args, input) -> str:
         if len(args) == 0 or len(args) > 1:
             raise ValueError("wrong number of command line arguments")
         os.chdir(args[0])
+        return ""
 
 
 class Ls(Application):
